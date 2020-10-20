6958df2e1acf0826d2eb0e6b09265b76ebeff7f8
Your NameKITAZAT0 <kevinbrandaobeauchard@gmail.com>
2020-10-20 09:25:40 +0200

    debut base todolit

--- appgamepad/src/App.js
+++ appgamepad/src/App.js
@@ -4,22 +4,34 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section id="todo">
+        <h1 class="m-3">Liste de tâches</h1>
+        <ul class="list-group m-3">
+            <li class="list-group-item d-flex align-tiems-center">
+                Ranger la vaisselle
+                <button class="btn btn-sm ml-auto btn-outline-success">&#x2713;</button>
+            </li>
+            <li class="list-group-item d-flex align-tiems-center">
+                Répondre appel d'offres
+                <button class="btn btn-sm ml-auto btn-outline-success">&#x2713;</button>
+            </li>
+            <li class="list-group-item d-flex align-tiems-center">
+                Signer contrat
+                <button class="btn btn-sm ml-auto btn-outline-success">&#x2713;</button>
+            </li>
+            <li class="list-group-item d-flex align-tiems-center">
+                Ranger le salon
+                <button class="btn btn-sm ml-auto btn-outline-success">&#x2713;</button>
+            </li>
+        </ul>
+        <footer class="d-flex justify-content-between bg-secondary p-3" id="mainFooter">
+            <div class="btn-group">
+                <a href="#" class="btn btn-outline-dark bg-light">List</a>
+                <a href="#" class="btn btn-outline-dark bg-light">Completed</a>
+                <a href="#" class="btn btn-outline-dark bg-light">Add</a>
+            </div>
+        </footer>
+    </section>
   );
 }
 
